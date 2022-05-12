b353b3f043c4957a120c0d7f6c7e766d211a4dee
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-05-12 17:45:47 -0500

    jumbotron alternative

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
 
 
-function Menu(props) {
-
-     
-   
-   const menu = props.dishes.map(dish => { 
+function RenderMenuItem({ dish, onClick }) {
    return (
-      <div key={dish.id} className="col-12 col-md-5 m-1" >
-         <Card onClick={() => props.onClick(dish)} >
+      <Card onClick={() => onClick(dish)} >
             <CardImg object src={dish.image} alt={dish.name} ></CardImg>
             <CardImgOverlay>
                <CardTitle>{dish.name}</CardTitle>  
             </CardImgOverlay>
          </Card>
+   )
+}
+
+function Menu(props) {     
+   const menu = props.dishes.map(dish => { 
+   return (
+      <div key={dish.id} className="col-12 col-md-5 m-1" >
+         <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
    )
 })
